d972451abfa5acd0d5c7e88a941ffa256dd405b9
kurepasu0731 <kurepasu0731@gmail.com>
2017-08-27 23:24:55 +0900

    データ送信方法を簡略化 holoの区別をなくしてみる

--- Assets/Resources/Scripts/SendData.cs
+++ Assets/Resources/Scripts/SendData.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Gogyo.Network;
 
 public class SendData : MonoBehaviour {
 
-    public RemoteTablet remoteTablet1;
-    public RemoteTablet remoteTablet2;
+    //public RemoteTablet remoteTablet1;
+    //public RemoteTablet remoteTablet2;
+
+    public RemoteTablet remoteTablet;
 
     // Use this for initialization
     void Start () {
@@ -19,8 +22,23 @@ public class SendData : MonoBehaviour {
 
     public void sendIDtoHolo(int idx, int idy)
     {
-        remoteTablet1.sendID(idx, idy);
-        remoteTablet2.sendID(idx, idy);
+        //remoteTablet1.sendID(idx, idy);
+        //remoteTablet2.sendID(idx, idy);
+
+        if (remoteTablet.Target.Count > 0)
+        {
+            string sendData = "{\"idX\":\"" + idx.ToString() + "\",  \"idY\":\"" + idy.ToString() + "\"}";
+            remoteTablet.Send(sendData);
+        }
+        //remoteTablet1.Send(sendData);
+        //remoteTablet2.Send(sendData);
+
+        //↑は以下と同じ
+        //foreach (PeripheralConnectedDevice d in remoteTablet.Target)
+        //{
+        //    string sendData = "{\"idX\":\"" + idx.ToString() + "\",  \"idY\":\"" + idy.ToString() + "\"}";
+        //    d.Send(sendData);
+        //}
 
     }
 }
